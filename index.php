fff09d8449b23fd2006847897c9268dc2c0ec1a7
Nathan Smith <nathanks@gmail.com>
2015-01-16 09:50:31 -0600

    added generic

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 
 $goog_user = 'ty0U45v4tzsZDXDl';
 $goog_pass = 'vEhiktac6MzizMAP';
-$domain = 'test.rowdyrides.com';
+$domain = 'your.domain.com';
 
+function checkLast($external){
+	//open txt
+	$handle = @fopen("lastip.txt", "r+");
+	$ip = fgets($handle);
+    	
+	if ($external != $ip){
+		rewind($handle);
+		$change = fwrite($handle, $external);
+		fclose($handle);
+		return true;
+	}else{
+		fclose($handle);
+		return false;		
+	}
+	
+}
 
 function getExternal(){
 	$externalContent = file_get_contents('http://checkip.dyndns.com/');
@@ -12,20 +28,26 @@ function getExternal(){
 }
 	
 $external = getExternal();
-$url = 'https://' . $goog_user . ':' . $goog_pass . '@domains.google.com/nic/update?hostname=' . $domain . '&myip=' . $external . '';
+$ipchanged = checklast($external);
 
-$ch = curl_init();
+if ($ipchanged){
+echo 'yes';
+	$url = 'https://' . $goog_user . ':' . $goog_pass . '@domains.google.com/nic/update?hostname=' . $domain . '&myip=' . $external . '';
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt ($ch, CURLOPT_CAINFO, "c:\cacert.pem");
+	$ch = curl_init();
 
-curl_exec($ch);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt ($ch, CURLOPT_CAINFO, "c:\cacert.pem");
 
-if($errno = curl_errno($ch)) {
-    $error_message = curl_strerror($errno);
-    echo "cURL error ({$errno}):\n {$error_message}";
+	curl_exec($ch);
+
+	if($errno = curl_errno($ch)) {
+		$error_message = curl_strerror($errno);
+		echo "cURL error ({$errno}):\n {$error_message}";
+	}
+
+	curl_close($ch);
 }
 
-curl_close($ch);
 ?>
